ee567b77397bda67ea6b5bcb112e963ef7bdcb0b
Ryotaro Kato <162657600+ryotarokato@users.noreply.github.com>
2024-05-09 13:20:13 +0800

    Update registerServiceWorker.js
    Update registerServiceWorker.js
    Co-authored-by: EmberGlow01 <superdev904@gmail.coom>

--- client/src/registerServiceWorker.js
+++ client/src/registerServiceWorker.js
@@ -1,6 +1,5 @@
 // In production, we register a service worker to serve assets from local cache.
 // This lets the app load faster on subsequent visits in production, and gives
-// cached resources are updated in the background.
 
 const isLocalhost = Boolean(
   window.location.hostname === 'localhost' ||
